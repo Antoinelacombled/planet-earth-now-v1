adaec8b0cf965f81cde94f8020960d68565c8467
benlac <benoit.lacombled@jellysmack.com>
2023-12-20 12:38:13 +0100

    feat: handle home links + remove react-scroll

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,5 @@
 import "./Header.css";
 import { Link, useLocation } from "react-router-dom";
-import { Link as ScrollLink } from "react-scroll";
 
 function Header() {
   const location = useLocation();
@@ -12,56 +11,27 @@ function Header() {
           Accueil
         </Link>
       )}
-      <a className="nav-menu-item">
-        <ScrollLink
-          to="getflag"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration={1500}
-        >
-          Obtenir le drapeau
-        </ScrollLink>
-      </a>
-      <a className="nav-menu-item">
-        <ScrollLink
-          to="principles"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration={1500}
-        >
-          Les principes
-        </ScrollLink>
-      </a>
-
-      <a className="nav-menu-item">
-        <ScrollLink
-          to="event"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration={1500}
-        >
-          Les événements & projets
-        </ScrollLink>
-      </a>
-
-      <a className="nav-menu-item">
-        <ScrollLink
-          to="contact"
-          spy={true}
-          smooth={true}
-          offset={-70}
-          duration={1500}
-        >
-          Contact
-        </ScrollLink>
-      </a>
-
-      <Link to={"/faq"} className="nav-menu-item">
-        FAQ
-      </Link>
+      {location.pathname === "/" && (
+        <>
+          <a className="nav-menu-item" href="#getflag">
+            Obtenir le drapeau
+          </a>
+          <a className="nav-menu-item" href="#principles">
+            Les principes
+          </a>
+          <a className="nav-menu-item" href="#event">
+            Les événements & projets
+          </a>
+          <a className="nav-menu-item" href="#contact">
+            Contact
+          </a>
+        </>
+      )}
+      {location.pathname !== "/faq" && (
+        <Link to={"/faq"} className="nav-menu-item">
+          FAQ
+        </Link>
+      )}
     </header>
   );
 }
