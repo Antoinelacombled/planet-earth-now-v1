ec835c38d8ae98993bd0da6fc19be432b5e44626
Antoine Lacombled <antoine.j.lacombled@gmail.com>
2023-12-21 17:55:45 +0100

    flag ang

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,23 @@
+import React, { useState } from "react";
 import "./Header.css";
 import { Link, useLocation } from "react-router-dom";
+import angflag from "../../assets/angflag.png"; // Assurez-vous que le chemin d'accès est correct
 
 function Header() {
   const location = useLocation();
+  const [showNotification, setShowNotification] = useState(false);
+
+  const handleFlagClick = () => {
+    setShowNotification(true);
+    setTimeout(() => {
+      setShowNotification(false);
+    }, 3000); // La notification disparaît après 3 secondes
+  };
 
   return (
     <header className="App-header">
+      {/* ...autres éléments du header... */}
+
       {location.pathname !== "/" && (
         <Link to={"/"} className="nav-menu-item">
           Accueil
@@ -31,6 +43,15 @@ function Header() {
         <Link to={"/faq"} className="nav-menu-item">
           FAQ
         </Link>
+      )}
+      <img
+        src={angflag}
+        alt="English flag"
+        className="english-flag"
+        onClick={handleFlagClick}
+      />
+      {showNotification && (
+        <div className="language-notification">ONLINE EARLY JANUARY 2024</div>
       )}
     </header>
   );
