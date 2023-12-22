a7befe66787cf8a882262138437ff9992c04fe61
Antoine Lacombled <antoine.j.lacombled@gmail.com>
2023-12-23 00:40:03 +0100

    flag

--- src/components/ClimateUrgencyBanner/ClimateUrgencyBanner.jsx
+++ src/components/ClimateUrgencyBanner/ClimateUrgencyBanner.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 import "./ClimateUrgencyBanner.css";
 
+import gifFlag from "../../assets/GIFFLAG.gif";
 import videoflag from "../../assets/1219.mov";
 
 function ClimateUrgencyBanner() {
   return (
     <section className="Climate-urgency-section">
-      <video
-        className="video-flag"
-        loop
-        muted
-        playsInline
-        preload="auto"
-        autoplay="autoplay"
-      >
+      <video className="video-flag desktop-only" loop autoPlay muted>
         <source src={videoflag} type="video/mp4" />
       </video>
+      <img className="gif-flag mobile-only" src={gifFlag} alt="Drapeau" />
     </section>
   );
 }
