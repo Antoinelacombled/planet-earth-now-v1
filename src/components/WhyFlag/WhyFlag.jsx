6421323fa99f88a417ce4cb1a82558b0a64965cd
benlac <benoit.lacombled@jellysmack.com>
2024-01-03 17:14:56 +0100

    feat: why the flag carousel

--- src/components/WhyFlag/WhyFlag.jsx
+++ src/components/WhyFlag/WhyFlag.jsx
@@ -1,12 +1,10 @@
-import React from "react";
 import { useTranslation } from "react-i18next";
-import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation } from "swiper/modules";
+import "./WhyFlag.css";
 import "swiper/css";
 import "swiper/css/navigation";
-import "./WhyFlag.css";
-
 SwiperCore.use([Navigation]);
 
 function WhyFlag() {
@@ -27,30 +25,35 @@ function WhyFlag() {
   return (
     <section className="why-flag-section">
       <h2 className="why-flag-title green">{t("WhyFlagTitle")}</h2>
-      <Swiper
-        navigation={{
-          nextEl: ".swiper-button-next-why-flag",
-          prevEl: ".swiper-button-prev-why-flag",
-        }}
-        spaceBetween={20}
-        slidesPerView={2.5}
-        slidesPerGroup={2}
-        className="why-flag-swiper"
-      >
-        {phrases.map((phrase, index) => (
-          <SwiperSlide
-            key={index}
-            className={`why-flag-text blue ${
-              index % 2 === 0 ? "even-class" : "odd-class"
-            }`}
+      <div className="container-box">
+        <div className="small-container-why-the-flag">
+          <Swiper
+            className="why-flag-swiper"
+            navigation={{
+              nextEl: ".swiper-button-next",
+              prevEl: ".swiper-button-prev",
+            }}
+            modules={[Navigation]}
+            spaceBetween={20}
+            slidesPerView={2.5}
+            slidesPerGroup={2}
           >
-            {phrase}
-          </SwiperSlide>
-        ))}
-      </Swiper>
-      <div className="why-flag-navigation">
-        <button className="swiper-button-prev-why-flag"> {"<"}</button>
-        <button className="swiper-button-next-why-flag"> {">"}</button>
+            {phrases.map((phrase, index) => (
+              <SwiperSlide
+                key={index}
+                className={`why-flag-text ${
+                  index % 2 === 0 ? "even-class" : "odd-class"
+                }`}
+              >
+                {phrase}
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </div>
+        <div>
+          <button className="swiper-button-prev test"> </button>
+          <button className="swiper-button-next"> </button>
+        </div>
       </div>
     </section>
   );
